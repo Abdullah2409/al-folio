284fd1acf7be47a4a7721b5fdfb9874d6efdcbc3
Abdullah2409 <97201253+Abdullah2409@users.noreply.github.com>
2025-12-01 00:10:31 +0000

    Deploying to gh-pages from @ Abdullah2409/Abdullah2409.github.io@e561c5bfdaf4de7ea539e4cfe98bb7c7bc0cac96 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "Abdullah2409/al-folio",
+    "data-repo": "Abdullah2409/Abdullah2409.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
